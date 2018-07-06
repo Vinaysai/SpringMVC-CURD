a146d9d8c561b8c37864107c3bc6ea7fed2211df
Vinay <ksvinaysai@gmail.com>
2018-07-06 23:04:59 +0530

    SpringMCV...CURD.

--- src/main/java/com/spring/dao/EmpDao.java
+++ src/main/java/com/spring/dao/EmpDao.java
@@ -4,7 +4,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
 
-import javax.swing.tree.RowMapper;
+import org.springframework.jdbc.core.RowMapper;
 
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
